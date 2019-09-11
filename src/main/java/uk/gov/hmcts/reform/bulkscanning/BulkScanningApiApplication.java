dd4f02334caff767208cf5984cd1ba0370053c45
Ayan1967421986 <Ayan.Dutta.1986@gmail.com>
2019-09-11 21:57:33 +0100

    Pay-2111: S2S with Bulk Scan API Implementation.

--- src/main/java/uk/gov/hmcts/reform/bulkscanning/BulkScanningApiApplication.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanning/BulkScanningApiApplication.java
@@ -2,15 +2,12 @@ package uk.gov.hmcts.reform.bulkscanning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import uk.gov.hmcts.reform.logging.spring.RequestLoggingAutoConfiguration;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "uk.gov.hmcts.reform.bulkscanning" },
-    excludeFilters = { @ComponentScan.Filter(
-        type = FilterType.ASSIGNABLE_TYPE,
-        value = { RequestLoggingAutoConfiguration.class }) })
+//@ComponentScan(basePackages = { "uk.gov.hmcts.reform.bulkscanning" },
+//    excludeFilters = { @ComponentScan.Filter(
+//        type = FilterType.ASSIGNABLE_TYPE,
+//        value = { RequestLoggingAutoConfiguration.class }) })
 public class BulkScanningApiApplication {
 
     public static void main(final String[] args) {
